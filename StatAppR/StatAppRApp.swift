e6a3dec4544ac131cbae813f111e59636353bf8e
yasuda <kusurix@gmail.com>
2026-03-04 11:01:45 +0900

    Initial Commit

---
+++ StatAppR/StatAppRApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatAppRApp.swift
+//  StatAppR
+//
+//  Created by uts on 2026/03/04.
+//
+
+import SwiftUI
+
+@main
+struct StatAppRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
